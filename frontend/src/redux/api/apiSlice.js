d82bf084b5adfc03a1431199688585eedc17d400
dushyant264 <ddushyant89@gmail.com>
2024-01-18 02:06:42 +0530

    clg 2

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -3,7 +3,7 @@ import { BASE_URL } from "../constants";
 
 
 const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL, prepareHeaders: (headers)=>{
-  const token = JSON.parse(localStorage.getItem('userInfo'));
+  const token = JSON.parse(localStorage.getItem('userInfo')).token;
   console.log(token)
   
     headers.set('authorization', `bearer ${token}`);
