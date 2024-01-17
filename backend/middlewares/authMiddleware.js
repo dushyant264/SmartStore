9ccc64880d83f5ed36857aa39e6a03088cb001b5
dushyant264 <ddushyant89@gmail.com>
2024-01-18 01:45:08 +0530

    undefined error

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -7,7 +7,8 @@ const authenticate = asyncHandler(async (req, res, next) => {
 
   // Read JWT from the 'jwt' cookie
   // token = req.cookies.jwt;
-  token = req.headers.authorization.split(" ")[1];
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(" ")[1];
 
   if (token) {
     try {
--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,9 +1,12 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constants";
-const token = JSON.parse(localStorage.getItem('userInfo')).token;
+
 
 const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL, prepareHeaders: (headers)=>{
-  headers.set('authorization', `bearer ${token}`);
+  const token = JSON.parse(localStorage.getItem('userInfo'))?.token;
+  if(token){
+    headers.set('authorization', `bearer ${token}`);
+  }
   return headers
 } });
 
